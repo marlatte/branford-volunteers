5e5a61be96012ca5878b037464fb78a0c16d14fd
MarLatte <86508134+marlatte@users.noreply.github.com>
2024-05-03 14:41:30 -0400

    Add dependencies & configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const tailwindConfig = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,12 +7,31 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        branford: {
+          50: "#f0f8ff",
+          100: "#e0f0fe",
+          200: "#bae2fd",
+          300: "#7cccfd",
+          400: "#37b3f9",
+          500: "#0d99ea",
+          600: "#0173bd",
+          700: "#0260a2",
+          800: "#065286",
+          900: "#0c456e",
+          950: "#082c49",
+        },
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "home-hero-wide": "url('/volunteers-hero.png')",
+        "home-hero-vert": "url('/volunteers-vert.png')",
+        "about-hero": "url('/kid-with-jacket.jpg')",
       },
     },
   },
   plugins: [],
 };
+
+export default tailwindConfig;
